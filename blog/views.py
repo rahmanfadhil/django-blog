b42d944284b4c3e864a23122422a0e1a9ebe0eb7
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-10-22 10:23:15 +0700

    Add data to templates

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Rahman Fadhil',
+        'title': 'Blog Post 1',
+        'content': 'First Post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second Post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
