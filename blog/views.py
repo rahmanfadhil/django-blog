0a33a1c4d91f0f770a4756d4d253d9f8f37a5852
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-10-22 20:59:20 +0700

    Add models and migrations

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Rahman Fadhil',
-        'title': 'Blog Post 1',
-        'content': 'First Post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second Post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
     }
     return render(request, 'blog/home.html', context)
 
